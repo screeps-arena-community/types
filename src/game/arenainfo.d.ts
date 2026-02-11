7fa5098cc68d32f4ab38cdaadd31581cc2065942
QzarGit <64895347+QzarGit@users.noreply.github.com>
2026-02-11 21:39:43 +0100

    ArenaInfo type added

---
+++ src/game/arenainfo.d.ts
@@ -0,0 +1,10 @@
+declare module 'game' {
+  export class arenaInfo {
+    readonly cpuTimeLimit: number
+    readonly cpuTimeLimitFirstTick: number
+    readonly level: number
+    readonly name: string
+    readonly season: string
+    readonly ticksLimit: number
+  }
+}
